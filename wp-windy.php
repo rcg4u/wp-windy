69683694be798d6f9bcdad980f246b6fa4b82ca2
rcg4u <rcg4u@users.noreply.github.com>
2025-03-31 23:27:41 -0500

    initial push of an idea
    I love using windy radar so i thought i would make one as a plugin for wordpress

---
+++ wp-windy.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: WP Windy Map
+Description: A plugin to display a Windy map using an iframe.
+Version: 1.0
+Author: Narcolepticnerd
+*/
+
+// Ensure this file is being run within the WordPress environment
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
